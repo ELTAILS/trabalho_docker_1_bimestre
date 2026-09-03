00e4933ccd59729368d66454fafdacf8faf12b68
Wagner Junior <wagnerjr306@gmail.com>
2026-09-03 19:04:42 -0300

    enviando tabela de ex

---
+++ backend/Database/produtos.sql
@@ -0,0 +1,6 @@
+INSERT INTO produtos 
+(nome, quantidade, marca, validade, preco)
+VALUES
+('Arroz', 100, 'Tio Negrão', '2026-10-30', 25.00),
+('Feijão', 80, 'Kicaldo', '2027-05-15', 8.50),
+('Macarrão', 50, 'Adria', '2027-08-20', 5.99);
