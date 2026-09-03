ded3965f11aa715a2a53f384a993dbcfa3fc97f0
Wagner da silva Junior <wagnerjr306@gmail.com>
2026-09-03 16:02:02 -0300

    Arrumei o index para o controller funcionar

--- frontend/index.php
+++ frontend/index.php
@@ -1,16 +1,32 @@
 <?php
 
+use App\Controller\ProdutoController;
+
 require_once __DIR__ . "/../vendor/autoload.php";
 
-try {
-    $database = new \App\Database\Database();
-    $pdo = $database->connect();
-    echo "Conexão bem-sucedida!";
-} catch (\Exception $e) {
-    echo "Erro: " . $e->getMessage();
-}
+define('BASE_URL', 'localhost:8080/frontend');
+
+$controller = new ProdutoController();
 
 $url = trim($_GET['url'] ?? '/');
+
+// switch($url){
+//     case '/':
+//         $controller->index();
+//         break;
+//     case 'produto':
+//         $controller->show((int)($_GET['id'] ?? 0));
+//         break;
+//     case 'produto/create':
+//         $controller->create();
+//         break;
+//     case 'produto/update':
+//         $controller->update((int)($_GET['id'] ?? 0));
+//         break;
+//     default:
+//         echo "pagina erro";
+//         break;
+// }
 
 switch($url){
     case '/':
