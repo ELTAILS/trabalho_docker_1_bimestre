8ac0daf73f0fd480d9d58db7dec4ea5b39d72f59
Wagner da silva Junior <wagnerjr306@gmail.com>
2026-09-04 11:10:27 -0300

    arrumando o envio de banco de dados

--- backend/Database/produtos.sql
+++ backend/Database/produtos.sql
@@ -1,3 +1,16 @@
+USE atacadao;
+
+CREATE TABLE produtos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    quantidade INT NOT NULL,
+    marca VARCHAR(50) NOT NULL,
+    validade DATE NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 INSERT INTO produtos 
 (nome, quantidade, marca, validade, preco)
 VALUES
